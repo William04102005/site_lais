267bd3ec6f0a5f469cef664536ab52306b92a9d3
William04102005 <williamcjunior400@gmail.com>
2024-08-02 21:52:21 -0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,71 +1,41 @@
-p {
-    margin: 0px;
-  }
+function cookies(functions) {
+    const container = document.querySelector('.cookies-container');
+    const save = document.querySelector('.cookies-save');
+    if (!container || !save) return null;
   
-  body {
-    margin: 0px;
-    height: 200vh;
-    background: #eee;
-  }
+    const localPref = JSON.parse(window.localStorage.getItem('cookies-pref'));
+    if (localPref) activateFunctions(localPref);
   
-  .cookies-container {
-    color: #b37f44;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    position: fixed;
-    width: 100%;
-    bottom: 2rem;
-    z-index: 1000;
-    font-size: 10px;
-  }
-  
-  .cookies-content {
-    box-shadow: 0 1px 3px #ebcaa8;
-    background: #ececc9;
-    max-width: 520px;
-    border-radius: 5px;
-    padding: 1rem;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 1fr auto;
-    gap: 0.5rem;
-    opacity: 0;
-    transform: translateY(1rem);
-    animation: slideUp 0.5s forwards;
-  }
-  
-  @keyframes slideUp {
-    to {
-      transform: initial;
-      opacity: initial;
+    function getFormPref() {
+      return [...document.querySelectorAll('[data-function]')]
+        .filter((el) => el.checked)
+        .map((el) => el.getAttribute('data-function'));
     }
-  }
   
-  .cookies-pref label {
-    margin-right: 1rem;
-  }
-  
-  .cookies-save {
-    grid-column: 2;
-    grid-row: 1/3;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: #F5F5DC;
-    color:  #b37f44;
-    cursor: pointer;
-    border: none;
-    border-radius: 5px;
-    padding: 0.8rem 1rem;
-    font-size: 1rem;
-  }
-  
-  @media (max-width: 500px) {
-    .cookies-content {
-      grid-template-columns: 1fr;
+    function activateFunctions(pref) {
+      pref.forEach((f) => functions[f]());
+      container.style.display = 'none';
+      window.localStorage.setItem('cookies-pref', JSON.stringify(pref));
     }
-    .cookies-save {
-      grid-column: 1;
-      grid-row: 3;
+  
+    function handleSave() {
+      const pref = getFormPref();
+      activateFunctions(pref);
     }
+  
+    save.addEventListener('click', handleSave);
   }
+  
+  function marketing() {
+    console.log('Função de marketing');
+  }
+  
+  function analytics() {
+    console.log('Função de analytics');
+  }
+  
+  cookies({
+    marketing,
+    analytics,
+  });
   
